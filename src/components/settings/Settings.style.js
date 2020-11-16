223f4a228fd28a7450784c8b74fd4d807a016175
qasimmehdi <qasim.sourcecode@gmail.com>
2020-11-16 11:08:48 +0500

    Logout added

--- src/components/settings/Settings.style.js
+++ src/components/settings/Settings.style.js
@@ -1,4 +1,13 @@
 import {StyleSheet} from 'react-native';
-import {COLOR} from './colors';
 
-export const settingsStyle = StyleSheet.create({});
+export const settingsStyle = StyleSheet.create({
+  button: {
+    flex: 1,
+    flexDirection: 'column-reverse',
+    marginBottom: 10,
+  },
+  topPart: {
+    flex: 5,
+    flexDirection: 'column',
+  },
+});
