d7e0e4ce74d4eae8b601646589ca2ac5ff76822f
qasimmehdi <qasim_127@hotmail.com>
2021-02-20 16:22:11 +0500

    sentiment page url added

--- src/components/news/sentiment.js
+++ src/components/news/sentiment.js
@@ -2,5 +2,5 @@ import React from 'react';
 import {WebView} from 'react-native-webview';
 
 export default function Sentiment() {
-  return <WebView source={{uri: 'http://10.0.2.2:3000/'}} />;
+  return <WebView source={{uri: 'http://twitter-embed.herokuapp.com/'}} />;
 }
